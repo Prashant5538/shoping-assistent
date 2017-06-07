8445334cc87dba4bbf56345cb1fa66a86e29a270
prashant00789 <prashant.singh@onedirect.in>
2017-06-07 15:34:06 +0530

    changes for result service

--- src/main/java/com/shopping/assistent/service/ResultServiceImpl.java
+++ src/main/java/com/shopping/assistent/service/ResultServiceImpl.java
@@ -2,7 +2,15 @@ package com.shopping.assistent.service;
 
 import org.springframework.stereotype.Service;
 
+import com.shopping.assistent.dto.ResultDTO;
+
 @Service
 public class ResultServiceImpl implements ResultService {
+
+	@Override
+	public ResultDTO fetchResult(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
